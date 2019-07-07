d13e8076454c30b5b0ee8864818415708af12386
Pedro Zaroni <zaronipedro@outlook.com>
2019-07-07 13:31:25 -0300

    adding bg image and bg containers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import WordArtText from './components/WordArtText';
 import WordArtThemes from './components/WordArtText/Themes';
@@ -6,11 +7,35 @@ import Gallery from './components/Gallery';
 
 // import './components/SparklyCursor';
 
+const bg = require('./images/header-bg.jpg');
+
+const Container = styled.div`
+  height: ${window.innerHeight}px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
+const TitleContainer = styled(Container)`
+  background-image: url(${bg});
+`;
+
 function App() {
   return (
     <>
-      <WordArtText text='Oi Pedro!' type={WordArtThemes.PURPLE} />
-      <Gallery />
+      <TitleContainer>
+        <WordArtText text='We love you Marina!' type={WordArtThemes.RAINBOW} />
+        <div style={{marginTop: '9em'}} />
+        <WordArtText text='Este site é destinado a guardar a lembrança dos ótimos momentos que vivemos juntxs aqui no Brasil até a corrente data de 12/07/2019.' fontSize='3em' type={WordArtThemes.PURPLE} />
+      </TitleContainer>
+      {/* <Gallery /> */}
     </>
   );
 }
--- src/components/WordArtText/index.js
+++ src/components/WordArtText/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import './wordart.css';
 
-// *Types*
-// - rainbow
-// - blues
-// - superhero
-// - radial
-// - purple
-
-// (https://www.designpieces.com/2017/03/wordart-css3-text-effects/)
-
-const WordArtText = ({ text, type }) => {
-    return (
-        <div class={`wordart ${type}`}><span class='text'>{text}</span></div>
-    );
-}
+const WordArtText = ({ text, type, fontSize }) => (
+  <div class={`wordart ${type}`} style={{fontSize: fontSize || '4em'}}>
+    <span class='text'>{text}</span>
+  </div>
+);
 
 export default WordArtText;
