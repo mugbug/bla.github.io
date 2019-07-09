b77356c7423c7c6b925203f100dc620354d64736
Julia Melo <julia.melo.ter@estacio.br>
2019-07-09 00:46:16 -0300

    organizing gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,13 +4,23 @@ import styled from 'styled-components';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const MyImage = styled.img`
-  width: 300px;
-  height: 300px;
+  width: 80%;
+  height: auto;
 `;
 
 const Gallery = () => {
-  const myImage = require('../images/image.png');
+  const myImage1 = require('../images/bdz 1.jpg');
+  const myImage2 = require('../images/bdz 2.jpg');
+  const myImage3 = require('../images/bdz 3.jpg');
+  const myImage4 = require('../images/bdz 4.jpg');
+  const myImage5 = require('../images/bdz 5.jpg');
 
   const stop = (e) => {
     e.preventDefault();
@@ -20,7 +30,6 @@ const Gallery = () => {
     <AliceCarousel
       mouseDragEnabled 
       dotsDisabled={false}
-      buttonsDisabled={true}
       responsive={{
         0: {
             items: 1
@@ -33,11 +42,21 @@ const Gallery = () => {
         }
       }}
     >
-      <MyImage src={myImage} onMouseDown={stop}/>
-      <MyImage src={myImage} onMouseDown={stop}/>
-      <MyImage src={myImage} onMouseDown={stop}/>
-      <MyImage src={myImage} onMouseDown={stop}/>
-      <MyImage src={myImage} onMouseDown={stop}/>
+      <ImageContainer>
+        <MyImage src={myImage1} onMouseDown={stop}/>
+      </ImageContainer>
+      <ImageContainer>
+        <MyImage src={myImage2} onMouseDown={stop}/>
+      </ImageContainer>
+      <ImageContainer>
+        <MyImage src={myImage3} onMouseDown={stop}/>
+      </ImageContainer>
+      <ImageContainer>
+        <MyImage src={myImage4} onMouseDown={stop}/>
+      </ImageContainer>
+      <ImageContainer>
+        <MyImage src={myImage5} onMouseDown={stop}/>
+      </ImageContainer>
     </AliceCarousel>
   );
 }
