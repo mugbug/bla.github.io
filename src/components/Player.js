672f3b1df1de036338066e5cda77f2b98e8463b3
Julia Melo <julia.melo.ter@estacio.br>
2019-07-11 12:40:14 -0300

    pubsub player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import PubSub from 'pubsub-js';
+
 import TransparentButton from './TransparentButton';
 
 const PlayerContainer = styled.div`
@@ -15,27 +17,50 @@ const PlayerContainer = styled.div`
 class Player extends React.Component {
   state = {
     play: false
-  }
+  };
 
   componentDidMount() {
     this.audio = new Audio(this.props.audio);
+
+    PubSub.subscribe('songPlayed', this.shouldStopSong);
 
     this.audio.onended = () => {
       this.setState({ play: false });
     };
   }
 
+  componentWillUnmount() {
+    PubSub.unsubscribe('songPlayed');
+  }
+
+  shouldStopSong = (e, songPlayed) => {
+    songPlayed !== this.props.audio && this.stopSong();
+  }
+  
+  asd = () => {
+    if (this.state.play) {
+      this.stopSong();
+    } else {
+      this.playSong();
+    }
+  };
+  
+  playSong = () => {
+    this.setState({ play: true });
+    this.audio.play();
+    PubSub.publish('songPlayed', this.props.audio);
+  };
+  
+  stopSong = () => {
+    this.setState({ play: false });
+    this.audio.pause();
+  };
+
   buttonIcon = () => (
     this.state.play
       ? "https://img.icons8.com/clouds/100/000000/pause.png"
       : "https://img.icons8.com/clouds/100/000000/play.png"
-  )
-
-  play = () => {
-    this.setState({ play: !this.state.play }, () => {
-      this.state.play ? this.audio.play() : this.audio.pause();
-    })
-  }
+  );
 
   getImageDesc = () => this.state.play ? 'play' : 'pause';
 
@@ -43,7 +68,7 @@ class Player extends React.Component {
     return (
       <PlayerContainer>
         <TransparentButton>
-          <img src={this.buttonIcon()} onClick={this.play} alt={this.getImageDesc()} />
+          <img src={this.buttonIcon()} onClick={this.asd} alt={this.getImageDesc()} />
         </TransparentButton>
       </PlayerContainer>
     );
